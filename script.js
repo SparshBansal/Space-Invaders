1db654d48f065253751640511d1068a97c0cc45f
sparsh <sparsh.bansal17895@gmail.com>
2016-07-05 07:17:51 +0530

    Add Interactions and Collision Handlers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var simpleLevelPlan = [
+gitvar simpleLevelPlan = [
   "x                            x",
   "x                            x",
   "x                            x",
@@ -17,15 +17,14 @@ var simpleLevelPlan = [
   "x                            x",
   "x                            x",
   "x                            x",
+  "x             @              x",
   "x                            x",
-  "x            @               x",
   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 ];
 
 /* TODO
--- Write the drawing mechanisms
--- Define the interactions
--- Handle collisions
+-- Listen to keypress events
+-- Put animation framework in place
 */
 
 // Vector constructor
@@ -106,7 +105,7 @@ Player.prototype.type = "player";
 
 // Constructor for Asteroid
 function Asteroid(position , type){
-  this.type = type;
+  this.asteroidType = type;
   if(type == "small"){
     this.size = new Vector(1.5,1.5);
     this.bulletsRequired = 1;
@@ -186,5 +185,197 @@ DOMDisplay.prototype.clear = function(){
   this.wrap.parentNode.removeChild(this.wrap);
 };
 
-var simpleLevel = new Level(simpleLevelPlan);
-var display = new DOMDisplay(document.body, simpleLevel);
+Level.prototype.obstacleAt = function(pos , size){
+  var minX = Math.floor(pos.x);
+  var maxX = Math.ceil(pos.x + size.x);
+  var minY = Math.floor(pos.y);
+  var maxY = Math.ceil(pos.y + size.y);
+
+  for(var i = minY ; i < maxY; i++){
+    for(var j = minX ; j < maxX; j++){
+      if(this.grid[i][j])
+        return this.grid[i][j];
+    }
+  }
+  return null;
+}
+
+Level.prototype.actorAt = function(actor){
+  for(var i=0 ; i<this.actors.length ; i++){
+    var other = actors[i];
+    if(actor! = other){
+      if(actor.pos.x + actor.size.x < other.pos.x ||
+        actor.pos.x < other.pos.x + other.size.x ||
+        actor.pos.y + actor.size.y < other.pos.y ||
+        actor.pos.y < other.pos.y + other.size.y){
+
+        }
+        else
+        return other;
+      }
+  });
+};
+
+var maxStep = 0.05;
+
+var type = {
+  0 : "small",
+  1 : "large"
+};
+
+Level.prototype.createAsteroid = function(){
+  var max = this.width - 1;
+  var min = 2;
+  var asteroidIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+  var asteroidType = type[Math.floor(Math.random() * (2)) + 0;]
+  var newAsteroid = new Asteroid(new Vector(asteroidIndex , -1) , asteroidType);
+  this.actors.push(newAsteroid);
+}
+
+Level.prototype.animate = function(step , keys){
+  while(step > 0){
+    if(this.status != null)
+    this.finishDelay -= step;
+
+    // If asteroid delay < 0 , then create a new asteroid
+    if(this.asteroidDelay < 0){
+      this.createAsteroid();
+      this.asteroidDelay = 0.3;
+    }else
+      this.asteroidDelay -= step;
+
+    var thisStep = Math.min(step , maxStep);
+    this.actors.forEach(function(actor){
+      actor.act(thisStep ,this , keys);
+    } , this);
+    step -= thisStep;
+  }
+};
+
+Asteroid.prototype.act = function(step , level){
+  var newPosition = this.pos.plus(this.speed.times(step));
+
+  // If there is an obstacle at this position then we remove the Asteroid
+  // Because it must have hit the bottom
+  if(level.obstacleAt(newPosition , this.size)){
+    level.actors = level.actors.filter(function(actor){
+      return this!= actor;
+    } , this);
+  }else
+    this.pos = newPosition;
+};
+
+Bullet.prototype.act = function(step , level){
+  var newPosition = this.pos.plus(this.speed.times(step));
+
+  if(level.obstacleAt(newPosition , this.size)){
+    console.log("Don't know how the f**k this happened");
+  }
+  this.pos = newPosition;
+  var collisionActor = actorAt(this);
+  if(collisionActor){
+    level.handleBulletCollision(this , collisionActor);
+  }
+};
+
+Level.prototype.handleBulletCollision = function(bullet , actor){
+  if(actor.type == "asteroid"){
+    actor.bulletsRequired -= 1;
+
+    // If the asteroid has been hit enough times destroy it
+    if(actor.bulletsRequired <= 0){
+      this.actors = this.actors.filter(function(other){
+        return actor!=other;
+      });
+    }
+  }else{
+    console.log("Now how did this happen");
+  }
+
+  // Remove the bullet from actors
+  this.actors = this.actors.filter(function(other){
+    return bullet != other;
+  });
+};
+
+// Handle players movements
+var playerSpeed = 7;
+
+// Handle horizontal movements
+Player.prototype.moveX = function(step , level , keys){
+  this.speed.x = 0;
+
+  if(keys.left)
+    this.speed.x = -playerSpeed;
+  if(keys.right)
+    this.speed.x = playerSpeed;
+
+  var speedX = new Vector(this.speed.x , 0);
+  var motion = speedX.times(step);
+  var newPosition = this.pos.plus(motion);
+
+  if(!level.obstacleAt(newPosition , this.size))
+    this.position = newPosition;
+  else{
+    // We have hit the wall , don't do anything
+  }
+};
+
+// Handle vertical movements
+Player.prototype.moveY = function(step , level , keys){
+  this.speed.y = 0;
+
+  if(keys.up)
+    this.speed.y = -playerSpeed;
+  if(keys.down)
+    this.speed.y = playerSpeed;
+
+  var speedY = new Vector(0 , this.speed.y);
+  var motion = speedY.times(step);
+  var newPosition = this.pos.plus(motion);
+
+  if(!level.obstacleAt(newPosition , this.size))
+  else{
+    // We have hit a wall , don't do anything
+  }
+};
+
+Level.prototype.createBullet = function(position){
+  var newBullet = new Bullet(position);
+  this.actors.push(newBullet);
+}
+
+// Handle movements , collisions with other actors , bullet firing
+Player.prototype.act = function(step , level , keys){
+  this.moveX(step , level , keys);
+  this.moveY(step , level , keys);
+
+  // Handle the player collisions
+  var collisionActor = level.actorAt(this);
+  if(collisionActor){
+    if(collisionActor.type == "asteroid"){
+      this.status = "lost";
+      this.finishDelay = 1;
+    }else{
+      // do nothing
+    }
+  }
+
+  // If status == lost , game over!!!
+  if(level.status == "lost"){
+    this.pos.y += step;
+    this.size.y -= step;
+  }
+
+  // Handle bullet firing
+  if(keys.space){
+    if(level.bulletDelay <= 0){
+      // fire the bullet
+      var pos = this.pos.plus(0 , -1);
+      level.createBullet(pos);
+      this.bulletDelay = 0.2;
+    }else{
+      this.bulletDelay -= step;
+    }
+  }
+};
